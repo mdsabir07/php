e6d4bbda6c642e718f20bd02c90803d51c35821f
Sabirul Islam <45106497+mdsabir07@users.noreply.github.com>
2023-04-03 20:43:01 +0600

    commit

--- module9/assignment/assignment-report.php
+++ module9/assignment/assignment-report.php
@@ -1,10 +1,8 @@
 <?php
 /*
+I created this minimal blogs using HTML, Tailwind CSS and PHP.
+There are 6 files (home.php, blog.php, single.php, contact.php, submit.php) including a responsive CSS file.
 
-
-Please submit the following materials for grading:
-
-1.A GitHub repository containing all the code for your blog.
-
-2. A written report detailing the design and implementation of your blog, including a description of any challenges you faced and how you overcame them.
+I faced problem to making the website responsive using Tailwind CSS. Since I'm a beginer in Tailwind CSS, I haven't experience in responsive design. But It will complete soon.
+That's why I added some custom responsive CSS. File name is responsive.css
 */
